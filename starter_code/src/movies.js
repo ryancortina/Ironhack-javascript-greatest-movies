20af707f088507ace04ec1bf9872baba6ed738bf
Ryan Cortina <22090@columbushs.com>
2019-11-22 08:45:36 -0500

    almost done

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,40 +1,81 @@
 /* eslint no-restricted-globals: 'off' */
 
 // Iteration 1: Ordering by year - Order by year, ascending (in growing order)
-movies.sort((a, b) => {
-    return a.year - b.year;
+function orderByYear(array){
+
+    let copy=[...array];
+    
+    copy.sort((a, b) => {
+        return a.year - b.year;
+        })
+       
+      
+    
+      movies.sort((a, b) => {
+        return a.year - b.year;
+        })
+       
+      movies.sort(orderByYear(a, b) =>{
+        if (a.year - b.year == 0) { 
+            if (a.title < b.title) { 
+                return -1;
+            }
+            if (a.title < b.title) {
+                return 1;
+            }
+            else {
+                return 0;
+            }
+        }
+        return a.year - b.year; 
     })
-  
-   console.log(movies.sort)
-   movies.sort(orderByYear(a, b) 
-    if (a.year - b.year == 0) { 
-        if (a.title < b.title) { 
-            return -1;
-        }
-        if (a.title < b.title) {
-            return 1;
-        }
-        else {
-            return 0;
-        }
-    }
-    return a.year - b.year; 
-})
-return copyOfArray; 
-};
+    
+    
   
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
 
-function howMnayMovies(anArray){
-    if(genre =="drama" && director == "steven speilberg"){
-        return
-    }
-}
-function howManyYears(anArray){
-    anArray.filter(check(anArray));
-}
+// function howMnayMovies(anArray){
+//     if(genre =="drama" && director == "steven speilberg"){
+//         return
+//     }
+// }
+// function howManyYears(anArray){
+//     anArray.filter(check(anArray));
+// }
 
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
+
+
+function orderAlphabetically(array){
+
+    let copy = [...array];
+    
+      copy.sort( (a, b) =>{
+        if(a.title < b.title){
+          return -1;
+        }
+        else if( b.title < a.title){
+          return 1;
+        }
+        else {
+          return 0;
+        }
+      })
+    
+      let first20 = copy.slice(0, 20) ;
+    
+    
+    
+      return first20;
+    
+    
+    }
+
+
+
+
+
+
 
 // Iteration 4: All rates average - Get the average of all rates with 2 decimals
 
